42477549e1e0753c99005fd6f541342f053056d0
Yehia Sakr <yehiasakr4676@gmail.com>
2025-01-23 16:27:29 +0200

    setup tailwind

---
+++ uni-front/tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        cgrey: "#E9EAED",
+        cblue: "#E0F0FF",
+        tcolor: "#344054",
+      },
+    },
+  },
+  plugins: [],
+};
